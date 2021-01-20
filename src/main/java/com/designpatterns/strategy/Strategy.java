4d036029011516a62f4408f7334bac255329f72d
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-20 16:08:59 +0000

    strategy

--- src/main/java/com/designpatterns/strategy/Strategy.java
+++ src/main/java/com/designpatterns/strategy/Strategy.java
@@ -4,14 +4,14 @@ public class Strategy {
 
     public static void execute() {
 
-        Context context = new Context(new OperationSum());
-        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+        StrategyOperation strategyOperation = new OperationSum();
+        System.out.println("10 + 5 = " + strategyOperation.doOperation(10, 5));
 
-        context = new Context(new OperationSubstract());
-        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+        strategyOperation = new OperationSubstract();
+        System.out.println("10 - 5 = " + strategyOperation.doOperation(10, 5));
 
-        context = new Context(new OperationMultiply());
-        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+        strategyOperation = new OperationMultiply();
+        System.out.println("10 * 5 = " + strategyOperation.doOperation(10, 5));
 
     }
 }
