3da7bbc3ac4aec6a71bbbce8af612c046c52761a
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-16 21:33:17 +0000

    insert description about Solid

--- src/main/java/com/designpatterns/solid/SolidExecute.java
+++ src/main/java/com/designpatterns/solid/SolidExecute.java
@@ -12,6 +12,27 @@ import java.time.Month;
 
 public class SolidExecute {
 
+    /**
+     * SOLID
+     * - Single Responsibility Principle
+     * - Open Closed Principle
+     * - Liskov Substitution Principle
+     * - Interface Segregation Principle
+     * - Dependency Inversion Principle
+     * <p>
+     * É interessante e mais seguro para o nosso código depender de interfaces
+     * (classes abstratas, assinaturas de métodos e interfaces em si) do que das implementações de uma classe
+     * <p>
+     * As interfaces são menos propensas a sofrer mudanças enquanto implementações podem mudar a qualquer momento
+     * <p>
+     * O Princípio de Inversão de Dependência (DIP) diz que implementações devem depender de abstrações e
+     * abstrações não devem depender de implementações
+     * <p>
+     * As interfaces devem definir apenas os métodos que fazem sentido para seu contexto
+     * <p>
+     * O Princípio de Segregação de Interfaces (ISP) diz que uma classe não deve ser obrigada a implementar um método que ela não precisa
+     */
+
     public static void execute() {
 
         EmployeeService employeeService = new EmployeeService();
